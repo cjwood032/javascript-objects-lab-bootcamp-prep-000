114aa5684ebea356e077cfbabd82f3516d25d338
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-04 20:03:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var recipes={}
+function updateObjectWithKeyAndValue(recipes, key, value) {
+  recipes[key]=value
+  return recipes
+}
+function deleteFromObjectByKey(recipes, key) {
+  delete recipes[key]
+  return recipes
+}
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  
+}
